c1feca6c105fe283ba7f68a466d9efb573b504ab
Karthik Medapalli <maxwhitie1234@gmail.com>
2025-07-27 19:08:32 +0530

    photo album initialisation

--- icon.py
+++ icon.py
@@ -4,9 +4,9 @@ root = Tk()
 root.title('Karthik')
 root.iconbitmap('C:/Users/maxwh/Desktop/Tkinter Course/images/icon.ico')
 
-#my_img = ImageTk.PhotoImage(Image.open('c'))
-#my_label = Label(image=my_img)
-#my_label.pack()
+my_img = ImageTk.PhotoImage(Image.open('images/asprin.png'))
+my_label = Label(image=my_img)
+my_label.pack()
 
 
 

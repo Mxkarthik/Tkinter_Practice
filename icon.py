30bf73a45a66b916b1fd321f2b894a095dc2df1d
Karthik Medapalli <maxwhitie1234@gmail.com>
2025-07-27 16:48:47 +0530

    method to add icon to the window

---
+++ icon.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from PIL import ImageTk,Image
+root = Tk()
+root.title('Karthik')
+root.iconbitmap('C:/Users/maxwh/Desktop/icon.ico')
+
+button_quit = Button(root, text="Exit Program" ,command=root.quit)
+button_quit.pack()
+
+
+
+
+
+
+root.mainloop()
